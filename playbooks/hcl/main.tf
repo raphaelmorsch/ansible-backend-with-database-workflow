f3164c5c75396b0f056a74e591828fe7a09e8917
Raphael Morsch <raphael@intersystem.eti.br>
2026-05-01 13:49:29 -0300

    adding provider to TF

--- playbooks/hcl/main.tf
+++ playbooks/hcl/main.tf
@@ -1,3 +1,16 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "db" {
+  ami           = "ami-0c02fb55956c7d316"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "demo-db"
+  }
+}
+
 output "db_vm_public_ip" {
   value = aws_instance.db.public_ip
 }
