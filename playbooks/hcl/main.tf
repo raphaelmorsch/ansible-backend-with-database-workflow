83c35d53d93441312ea5e1907700e8c946c51f31
Raphael Morsch <raphael@intersystem.eti.br>
2026-05-01 13:54:13 -0300

    #3 figuring out AMI based on region

--- playbooks/hcl/main.tf
+++ playbooks/hcl/main.tf
@@ -2,8 +2,28 @@ provider "aws" {
   region = "us-east-2"
 }
 
+data "aws_ami" "rhel9" {
+  most_recent = true
+  owners      = ["309956199498"] # Red Hat
+
+  filter {
+    name   = "name"
+    values = ["RHEL-9*_HVM-*-x86_64-*"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
 resource "aws_instance" "db" {
-  ami           = "ami-0c02fb55956c7d316"
+  ami           = data.aws_ami.rhel9.id
   instance_type = "t3.micro"
 
   tags = {
