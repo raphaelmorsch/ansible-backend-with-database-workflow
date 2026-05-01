72d393727d0e8901d2d8a0acbbc44dc39fa467f2
Raphael Morsch <raphael@intersystem.eti.br>
2026-05-01 18:26:51 -0300

    #32 Adding EE for PostgreSQL + adding PORT 22 + 5432 liberation on AWS VPC

--- playbooks/hcl/main.tf
+++ playbooks/hcl/main.tf
@@ -30,10 +30,55 @@ variable "aws_key_name" {
   type = string
 }
 
+variable "ssh_allowed_cidr" {
+  type    = string
+  default = "0.0.0.0/0" # demo apenas
+}
+
+variable "postgres_allowed_cidr" {
+  type    = string
+  default = "0.0.0.0/0" # demo apenas
+}
+
+resource "aws_security_group" "db_sg" {
+  name        = "${var.vm_name}-sg"
+  description = "Allow SSH and PostgreSQL for demo"
+
+  ingress {
+    description = "SSH from AAP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.ssh_allowed_cidr]
+  }
+
+  ingress {
+    description = "PostgreSQL from OpenShift"
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = [var.postgres_allowed_cidr]
+  }
+
+  egress {
+    description = "Allow outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name      = "${var.vm_name}-sg"
+    ManagedBy = "Terraform"
+  }
+}
+
 resource "aws_instance" "db" {
   ami           = data.aws_ami.rhel9.id
   instance_type = "t3.micro"
   key_name      = var.aws_key_name
+  vpc_security_group_ids = [aws_security_group.db_sg.id]
 
   tags = {
     Name = var.vm_name
